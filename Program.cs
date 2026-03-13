e41ac0030af1d15098ff821c70203587f05dc6e3
Tuki <martreye@ucm.es>
2026-03-13 12:14:01 +0100

    Añadido el programa principal

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Marta Reyes Funk
+// Diego Gonzalez Martin
+// (Hermes)
+
+namespace FP2_Practica02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
