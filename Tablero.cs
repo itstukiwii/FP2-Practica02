3a8e190c33009efc3c35f4bc2e9e2551793b42db
Tuki <martreye@ucm.es>
2026-03-26 10:20:31 +0100

    Añadido clase Coor y clase Tablero

---
+++ Tablero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Coordinates;
+
+namespace Tablero
+{
+    class Tablero
+    {
+        // casillas de juego
+        private struct Casilla
+        {
+            public char estado; // "o" sin descubir, "*" mina, "x" bandera
+                                // "·" sin minas alrededor, "1" a "8" número de minas alrededor
+            public bool mina; // true - hay mina ; false - no hay mina
+        }
+        private int fils, cols;         // número de filas y columnas del tablero
+        private Casilla[,] casilla;     // matriz de casillas del tablero
+        private Coor cursor;            // posición del cursor (fila, columna)
+        private int nMinas, nMarcadas;  // número de minas y número de casillas marcadas con bandera
+        private bool primerClick;       // para garantizar que el primer click no sea una mina
+        private bool debug;            // para depuración, el renderizado muestra el tablero con las minas
+        static Random rand = new Random(); // generador de aleatorios para colocar minas
+    }
+}
