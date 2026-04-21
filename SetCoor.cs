43a41de6b81e610d7cf3fce6da2e1fca49fa3bda
Hermes022 <Hermescaracol@gmail.com>
2026-04-21 11:58:09 +0200

    esa cosa importante que no hemos usado nunca

---
+++ SetCoor.cs
@@ -0,0 +1,54 @@
+﻿//Diego Gonzalez Martin
+//Marta Reyes Funk
+namespace Hoja5;
+using Coordinates;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+class SetCoor
+{
+    Coor[] coors;
+    int oc;
+    public SetCoor (int tam = 10)
+    {
+        coors = new Coor[tam];
+        oc = 0;
+    }
+    private int SearchElem (Coor c)
+    {
+        bool enc = false;
+        int n = 0;
+        while (n<coors.Length&&!enc)
+        {
+            if (coors[n] == c) enc = true;
+            else n++;
+        }
+        if (enc) { return n; }
+        else { return -1; }
+    }
+    public bool Add(Coor c)
+    {
+        bool existe = SearchElem(c)!=-1;
+        if (!existe)
+        {
+            if (oc != coors.Length)
+            {
+                coors[oc] = c;
+                oc++;
+            }
+        else Console.WriteLine("error, array lleno");
+        }
+        return !existe;
+    }
+    public bool Belongs (Coor c)
+    {
+        return SearchElem(c)!=-1;
+    }
+}
+
